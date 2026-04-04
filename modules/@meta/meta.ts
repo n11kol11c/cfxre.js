a79217ed42df248fe0a8cc4247aee71ede584140
м <174154109+n11kol11c@users.noreply.github.com>
2026-04-05 01:28:41 +0200

    Update meta.ts

--- modules/@meta/meta.ts
+++ modules/@meta/meta.ts
@@ -1,9 +1,12 @@
+import { AppError } from "@exception/errors";
+
 let versionCache: string = "1.0.0";
+let authorCache: string = "Matija";
 
 /**
  * Validates and sets the version string.
  */
-function setCurrentVersion(v: any): string {
+function setCurrentVersion(v: string): string {
     if (typeof v === 'string') {
         versionCache = v;
         return v;
@@ -23,3 +26,28 @@ export const getCurrentVersion = (): string => {
     }
     return "0.0.1-alpha";
 };
+
+/**
+ * Validates and sets the author.
+ */
+function setCurrentAuthor(author: string): string {
+    if (typeof author === 'string') {
+        authorCache = author;
+        return author;
+    } else {
+        throw new AppError(`[cfxjs] Invalid data type passed to setCurrentAuthor. Expected string, got ${typeof author}`)
+    }
+}
+
+/**
+ * Returns the current library author.
+ */
+export const getCurrentAuthor = (): string => {
+    const author = setCurrentAuthor(authorCache);
+
+    if (author) {
+        return author;
+    }
+
+    return "n11kol11c";
+}
