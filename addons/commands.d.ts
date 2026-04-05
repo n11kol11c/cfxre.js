4b96d3b51416bbf9deed8da15572afa5507c0ccb
м <174154109+n11kol11c@users.noreply.github.com>
2026-04-05 19:28:57 +0200

    Create commands.d.ts

---
+++ addons/commands.d.ts
@@ -0,0 +1,11 @@
+/**
+ * A static list of internal command identifiers registered by the cfxre.js library.
+ * * @description
+ * These commands are automatically initialized within the Cfx.re runtime 
+ * to provide core utility functions such as version checking and time synchronization.
+ * * @example
+ * // getversion   - Displays current library version
+ * // getauthor    - Displays library author information
+ * // gta:settime  - Updates the in-game world time
+ */
+export declare const AddonCommandsList: readonly string[];
