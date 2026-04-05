2873bf9f967db97df4db425a3ad46484a6f49fd1
м <174154109+n11kol11c@users.noreply.github.com>
2026-04-05 19:42:27 +0200

    Update cfxre.js

--- cfxre.js
+++ cfxre.js
@@ -1,9 +1,9 @@
-import { CfxMarker, CfxMarkerSystem } from '@modules/markers/markers.js';
-import { CfxAuth } from '@modules/markers/markersauth.js';
-import { CfxRegisterESXCommand, CfxRegisterQboxCommand } from '@modules/commands/registercommands.js';
-import { Blips } from '@modules/blips/blips.js';
-import { CfxPromise } from '@modules/@syncpromise/promise';
-import { getCurrentVersion, getCurrentAuthor } from '@modules/@meta/meta';
+import { CfxMarker, CfxMarkerSystem } from '@cfxmodules/markers/markers.js';
+import { CfxAuth } from '@cfxmodules/markers/markersauth.js';
+import { CfxRegisterESXCommand, CfxRegisterQboxCommand } from '@cfxmodules/commands/registercommands.js';
+import { Blips } from '@cfxmodules/blips/blips.js';
+import { CfxPromise } from '@cfxmodules/@syncpromise/promise';
+import { getCurrentVersion, getCurrentAuthor } from '@cfxmodules/@meta/meta';
 
 /**
  * @type {import('@cfxtypes').CfxjsLibrary} Cfxjs
